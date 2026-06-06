2ea0bfbc3c7aa66d4f0c4bab18d21e88e344852e
RasmusOlesk <rasmus.olesk12@gmail.com>
2026-06-07 01:25:21 +0300

    Vite config eemaldasin babel

--- Veebileht_SPA/my-app/vite.config.ts
+++ Veebileht_SPA/my-app/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rollup/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
   base: '/Interaktiivne_veebileht_SPA/'
 })
